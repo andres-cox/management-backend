6172240babf560bb4d3aea53bf3582ca9946880d
andres-cox-mojix-com <andreshcox@gmail.com>
2019-10-22 17:18:55 -0300

    linked db

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 //Connecting Database
+//deployment
 //const url = "mongodb://localhost:27017/employment";
-const url = "mongodb://mongo:27017/employment";
+//docker
+//const url = "mongodb://mongo:27017/employment";
+//deployment
+const url =
+  "mongodb://andres:andrespass@cluster0-shard-00-00-breqi.mongodb.net:27017,cluster0-shard-00-01-breqi.mongodb.net:27017,cluster0-shard-00-02-breqi.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(url, { useNewUrlParser: true });
 mongoose.connection.once("open", () =>
   console.log(`Connected to mongo at ${url}`)
 );
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Apollo Server
-const { typeDefs } = require("../apollo/type-definitions");
-const { resolvers } = require("../apollo/resolvers");
+const { typeDefs } = require("./apollo/type-definitions");
+const { resolvers } = require("./apollo/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
